264c4951cae9b5a7ce0211ca9122819422dacc9c
rahulvijay81 <rahulvjay8156@gmail.com>
2023-03-15 12:24:47 +0530

    contactpage description

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -7,7 +7,7 @@ function Contactpage() {
     <div className='contactbackground'>
       <Container>
         <h2 className='contacthead'>Get In Touch</h2>
-        <p className='contactpara'>Although I’m not currently looking for any new opportunities, my inbox <br /> is always open. Whether you have a question or just want to say hi, I’ll try <br /> my best to get back to you!</p>
+        <p className='contactpara'> I’m currently searching for opportunities for a front-end developer role. <br /> If there is any vacancy my inbox is always open. Whether <br /> you have any further questions or just want to say hi, <br /> I’ll try my best to get back to you!</p>
         <button
           className='contactbtn'
           onClick={() => {
