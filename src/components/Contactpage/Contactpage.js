25d3f044c0d4720890a930f4c26f740fe46de964
rahulvijay81 <rahulvijay8156@gmail.com>
2023-05-10 23:50:30 +0530

    contact update

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import '../../pages/style.css';
-import { Container } from 'react-bootstrap'
+import React from "react";
+import "../../pages/style.css";
+import { Container } from "react-bootstrap";
 
 function Contactpage() {
   return (
-    <div className='contactbackground'>
+    <div className="contactbackground">
       <Container>
-        <h2 className='contacthead'>Get In Touch</h2>
-        <p className='contactpara'> I’m currently searching for opportunities for a front-end developer role. <br /> If there is any vacancy my inbox is always open. Whether <br /> you have any further questions or just want to say hi, <br /> I’ll try my best to get back to you!</p>
+        <h2 className="contacthead">Get In Touch</h2>
+        <p className="contactpara">
+          I’m currently searching for opportunities for a front-end developer
+          role. <br /> If there is any vacancy my inbox is always open. Whether{" "}
+          <br /> you have any further questions or just want to say hi, <br />{" "}
+          I’ll try my best to get back to you!
+        </p>
         <button
-          className='contactbtn'
+          className="contactbtn"
           onClick={() => {
-            window.open("https://wa.me/+918156811884");
+            window.open("https://www.instagram.com/rahulvijay81/");
           }}
-        >Say Hello</button>
+        >
+          Say Hello
+        </button>
         <span></span>
-        <p className='copyright'>© Copyright 2023
-        <hr />
-        Designed & Built by <span>rahulvijay</span></p>
+        <p className="copyright">
+          © Copyright 2023
+          <hr />
+          Designed & Built by <span>rahulvijay</span>
+        </p>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Contactpage
+export default Contactpage;
