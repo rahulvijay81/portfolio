0811e362666d550551c8ddb4e6d8266b17ee69f1
rahulvijay81 <rahulvijay8156@gmail.com>
2023-05-10 23:52:49 +0530

    Contactpage update

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -9,8 +9,8 @@ function Contactpage() {
         <h2 className="contacthead">Get In Touch</h2>
         <p className="contactpara">
           I’m currently searching for opportunities for a front-end developer
-          role. <br /> If there is any vacancy my inbox is always open. Whether{" "}
-          <br /> you have any further questions or just want to say hi, <br />{" "}
+          role. <br /> If there is any vacancy my inbox is always open. Whether
+          <br /> you have any further questions or just want to say hi, <br />
           I’ll try my best to get back to you!
         </p>
         <button
