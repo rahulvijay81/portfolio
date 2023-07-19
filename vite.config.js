adeaaef6b136bdf6c7eefb272e70cb65ecf7250b
rahulvijay81 <rahulvijay8156@gmail.com>
2023-07-19 16:42:48 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    // include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
-    },
-  },
-});
+})
