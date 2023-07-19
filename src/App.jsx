3587e81366f9f878943f1a3d700f40cff97928ee
rahulvijay81 <rahulvijay8156@gmail.com>
2023-07-19 20:03:23 +0530

    custom cursor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import Preloader from '../src/components/Pre'
-import Home from './pages/Home'
-import About from './pages/About'
+import Preloader from '../src/components/Pre';
+import Home from './pages/Home';
+import About from './pages/About';
 import Project from './pages/Project';
 import Contact from './pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
+import Cursor from '../src/components/Cursor';
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0
-  });
-  console.log(mousePosition);
-
-  useEffect(() => {
-    const mouseMove = e => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY
-      })
-    }
-
-    window.addEventListener("mousemove", mouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-    }
-  }, []);
-
-  const variants = {
-    default: {
-      x: mousePosition.x - 16,
-      y: mousePosition.y - 16
-    }
-  }
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
@@ -49,24 +22,20 @@ function App() {
 
   return (
     <div className="App">
-      <motion.div
-        className="cursor"
-        variants={variants}
-        animate="default"
-      />
+      <Cursor />
       <Router>
         <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <div className="App" id={load ? 'no-scroll' : 'scroll'}>
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/project' element={<Project />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
