7ac8e73bfbf65f3e6584ddb489686155395312b5
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-02 15:55:49 +0530

    Header UI updated

--- src/components/Contactpage.jsx
+++ src/components/Contactpage.jsx
@@ -1,11 +1,11 @@
-import { Container } from "react-bootstrap";
+
 import "../pages/style.css";
 
 function Contactpage() {
 
   return (
     <div className="contactbackground">
-      <Container>
+      <div>
         <h2 className="contacthead">Get In Touch</h2>
         <p className="contactpara">
           I'd love to connect and explore exciting opportunities with you! Whether you have interesting projects, creative ideas, or just want to chat, please don't hesitate to reach out. My inbox is open 24/7!
@@ -20,7 +20,7 @@ function Contactpage() {
         </button>
         <span></span>
      
-      </Container>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,56 @@
 import { useEffect, useState } from "react";
-import logo from "../Assets/images/logor.png"
-
+import logo from "../Assets/images/logor.png";
 
 const Header = () => {
-const [isScrolling , setisScrolling] = useState(false);
+  const [isScrolling, setisScrolling] = useState(false);
 
-useEffect(() => {
-  const handleScroll = () => {
-    const scrollTop = window.scrollY
-    const isCurrentScrolled =  scrollTop > 0 ;
-    setisScrolling(isCurrentScrolled)
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const isCurrentScrolled = scrollTop > 0;
+      setisScrolling(isCurrentScrolled);
+    };
 
-  window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-},[]);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className={`flex items-center justify-between w-full px-28 pt-2 fixed top-0 z-50 transition-all text-base ${isScrolling ? 'sticky' : ''}`}>
+    <header
+      className={`flex items-center justify-between w-full px-28 pt-2 fixed top-0 z-50 transition-all text-base ${
+        isScrolling ? "sticky" : ""
+      }`}
+    >
       <div>
-       <img className="w-[90px] h-[80px] bg-no-repeat bg-cover" src={logo} alt="" />
+        <img
+          className="w-[90px] h-[80px] bg-no-repeat bg-cover"
+          src={logo}
+          alt=""
+        />
       </div>
       <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="/" className="text-white hover:text-gray-300">Home</a>
-          </li>
-          <li>
-            <a href="/about" className="text-white hover:text-gray-300">About</a>
-          </li>
-          <li>
-            <a href="/contact" className="text-white hover:text-gray-300">Projects</a>
-          </li>
-          <li>
-            <a href="/contact" className="text-white hover:text-gray-300">Contact</a>
-          </li>
-          <li>
-            <button className="w-[120px bg-blue-900 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        <div className="flex items-center space-x-4 gap-6">
+          <div>
+            <p className="text-white hover:text-gray-900">Home</p>
+          </div>
+          <div>
+            <p className="text-white hover:text-gray-900">About</p>
+          </div>
+          <div>
+            <p className="text-white hover:text-gray-900">Projects</p>
+          </div>
+          <div>
+            <p className="text-white hover:text-gray-900">Contact</p>
+          </div>
+          <div>
+            <button className="w-[120px] button-UI text-gray-900 hover:text-white font-bold py-1.5 px-4 rounded-lg tracking-wider">
               Resume
             </button>
-          </li>
-        </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
