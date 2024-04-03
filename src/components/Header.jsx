c097ea70268a3d4405aeafe743123bc5c548856e
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-03 12:24:21 +0530

    header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import logo from "../Assets/images/logor.png";
+import { Link, Events, scrollSpy } from "react-scroll";
 
 const Header = () => {
   const [isScrolling, setisScrolling] = useState(false);
+  const [activeSection, setActiveSection] = useState(null);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -12,9 +14,15 @@ const Header = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
+    Events.scrollEvent.register("begin", function (to) {
+      setActiveSection(to);
+    });
+
+    scrollSpy.update();
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      Events.scrollEvent.remove("begin");
     };
   }, []);
 
@@ -33,18 +41,78 @@ const Header = () => {
       </div>
       <nav>
         <div className="flex items-center space-x-4 gap-6">
-          <div>
-            <p className="text-white hover:text-gray-900">Home</p>
-          </div>
-          <div>
-            <p className="text-white hover:text-gray-900">About</p>
-          </div>
-          <div>
-            <p className="text-white hover:text-gray-900">Projects</p>
-          </div>
-          <div>
-            <p className="text-white hover:text-gray-900">Contact</p>
-          </div>
+          <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-250}
+            duration={500}
+            to="Home-section"
+          >
+            <p
+              className={
+                activeSection === "Home-section"
+                  ? "active"
+                  : "text-white hover:text-gray-900"
+              }
+            >
+              Home
+            </p>
+          </Link>
+          <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+            to="About-section"
+          >
+            <p
+              className={
+                activeSection === "About-section"
+                  ? "active"
+                  : "text-white hover:text-gray-900"
+              }
+            >
+              About
+            </p>
+          </Link>
+          <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+            to="Project-section"
+          >
+            <p
+              className={
+                activeSection === "Project-section"
+                  ? "active"
+                  : "text-white hover:text-gray-900"
+              }
+            >
+              Projects
+            </p>
+          </Link>
+          <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            to="Contact-section"
+          >
+            <p
+              className={
+                activeSection === "Contact-section"
+                  ? "active"
+                  : "text-white hover:text-gray-900"
+              }
+            >
+              Contact
+            </p>
+          </Link>
           <div>
             <button className="w-[120px] button-UI text-gray-900 hover:text-white font-bold py-1.5 px-4 rounded-lg tracking-wider shadow-xl">
               Resume
